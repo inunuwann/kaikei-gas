a9cf9af21fc9aee4ca7476190770864980018cfe
inunuwann <yukiya.0216.5063@gmail.com>
2026-03-18 19:23:30 +0900

    fix actions

--- build.js
+++ build.js
@@ -1,9 +1,48 @@
+const fs = require('node:fs/promises');
+const path = require('node:path');
 const { build } = require('esbuild');
 const { GasPlugin } = require('esbuild-gas-plugin');
 
-build({
-  entryPoints: ['src/code.ts'],
-  bundle: true,
-  outfile: 'dist/code.js',
-  plugins: [GasPlugin],
-}).catch(() => process.exit(1));
+const DIST_DIR = 'dist';
+const SRC_DIR = 'src';
+const STATIC_ASSETS = ['appsscript.json'];
+
+async function main() {
+  await fs.mkdir(DIST_DIR, { recursive: true });
+
+  await build({
+    entryPoints: [path.join(SRC_DIR, 'code.ts')],
+    bundle: true,
+    outfile: path.join(DIST_DIR, 'code.js'),
+    plugins: [GasPlugin],
+  });
+
+  await copyHtmlAssets();
+  await copyStaticAssets();
+}
+
+async function copyHtmlAssets() {
+  const entries = await fs.readdir(SRC_DIR, { withFileTypes: true });
+  const htmlFiles = entries
+    .filter((entry) => entry.isFile() && entry.name.endsWith('.html'))
+    .map((entry) => entry.name);
+
+  await Promise.all(
+    htmlFiles.map((fileName) =>
+      fs.copyFile(path.join(SRC_DIR, fileName), path.join(DIST_DIR, fileName)),
+    ),
+  );
+}
+
+async function copyStaticAssets() {
+  await Promise.all(
+    STATIC_ASSETS.map((fileName) =>
+      fs.copyFile(path.join(SRC_DIR, fileName), path.join(DIST_DIR, fileName)),
+    ),
+  );
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
